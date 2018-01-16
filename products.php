e14c74473649dfa1640ca3008c64818bab9e0db2
TripTD <34335437+TripTD@users.noreply.github.com>
2018-01-16 17:34:30 +0200

    Update products.php

--- products.php
+++ products.php
@@ -22,7 +22,7 @@
         }
     }
 
-    $query = "SELECT id, title, description, price FROM products ORDER BY id";
+    $query = "SELECT id, title, description, price, imeg FROM products ORDER BY id";
     if($stmt = $conn->prepare($query)) {
         $stmt->execute();
         $result = $stmt->get_result();
@@ -35,23 +35,27 @@
         </head>
         <body>
             <div id="container">
+                <?php echo t('Language preference') .":" ; ?>
+                <p><a href = "products.php"><?php echo t('English'); ?></a></p>
+                <p><a href = "products.php?l=fr"><?php echo t('Francais'); ?></a></p>
                 <div id="main">
                     <table>
                         <?php while ( $row = $result->fetch_array(MYSQLI_NUM)): ?>
                             <tr>
+                                <td><img width = "200" src = "<?php echo $row[4]; ?>" alt = ""></td>
                                 <td><?php echo $row[1]; ?></td>
                                 <td><?php echo $row[2]; ?></td>
                                 <td><?php echo $row[3]; ?></td>
-                                <td><a href="Product.php?page=products&action=edit&id=<?php echo $row[0] ?>"> Edit item </a></td>
-                                <td><a href="Products.php?page=products&action=remove&id=<?php echo $row[0] ?>"> Remove item </a></td>
+                                <td><a href="Product.php?page=products&action=edit&id=<?php echo $row[0] ?>"><?php echo t('Edit Item'); ?></a></td>
+                                <td><a href="Products.php?page=products&action=remove&id=<?php echo $row[0] ?>"><?php echo t('Remove Item'); ?></a></td>
                             </tr>
                         <?php endwhile; ?>
                     </table>
                     <br>
                 </div>
-                <p><a href="Product.php?page=products&action=insert"> Add item </a></p>
+                <p><a href="Product.php?page=products&action=insert"><?php echo t('Add Item'); ?></a></p>
                 <br>
-                <p><a href="Products.php?page=products&action=logout"> Log out</a></p>
+                <p><a href="Products.php?page=products&action=logout"><?php echo t('Log out'); ?></a></p>
             </div>
         </body>
     </html>
