6250cc647101058d692195e47b3ae6c7c34881b4
TripTD <34335437+TripTD@users.noreply.github.com>
2018-01-26 15:17:09 +0200

    updated

--- cart.php
+++ cart.php
@@ -2,64 +2,97 @@
 
     require("common.php");
 
-    //function for validation
-    function valid_email($email) {
-        return !!filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
 
     //checking for product id to remove it from the cart array
     if (isset($_GET["id"]) && intval($_GET["id"])) {
         $id_prod = intval($_GET["id"]);
-        $key = array_search($id_prod,$_SESSION["cart"]);
-        if ($key !== false) {
-            unset($_SESSION["cart"][$key]);
+        $cart_key = array_search($id_prod,$_SESSION["cart"]);
+        if ($cart_key !== false) {
+            unset($_SESSION["cart"][$cart_key]);
         }
         $_SESSION["cart"] = array_values($_SESSION["cart"]);
     }
 
-    //getting all the data into an array which will be used to display the cart and to send the products through the mail
-    if (!isset($_SESSION["products"])) {
-        $_SESSION["products"] = array();
-        $sql = "SELECT * FROM products";
+    //if $_SESSION["cart"] is not empty get the data from the database
+    if (count($_SESSION["cart"]) > 0) {
+
+        //binding the parameters and selection part of the INDEX
+        // type will be the data type for binding and params will be the reference array of the cart products
+        if (!isset($product)) {
+            $product = array();
+        }
+        if (!isset($type) && !isset($params)) {
+            $type = "";
+            $params = array();
+        }
+
+        for ($i = 0; $i < count($_SESSION["cart"]); $i++) {
+            $type .= "s";
+            $params[] = & $_SESSION["cart"][$i];
+        }
+
+        $sql = "SELECT * FROM products WHERE id IN (".str_repeat('?,',count($_SESSION['cart'])-1).'?'.")";
         if ($stmt = $conn->prepare($sql)) {
+            call_user_func_array(array($stmt,"bind_param"), array_merge(array($type),$params));
             $stmt->execute();
             $result = $stmt->get_result();
-            $index_prod = 0;
+            $index = 0;
             while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                $_SESSION["products"][$index_prod]["id"] = $row["id"];
-                $_SESSION["products"][$index_prod]["title"] = $row["title"];
-                $_SESSION["products"][$index_prod]["description"] = $row["description"];
-                $_SESSION["products"][$index_prod]["price"] = $row["price"];
-                $_SESSION["products"][$index_prod]["img"] = $row["img"];
-                $index_prod++;
+                $product[$index]["id"] = $row["id"];
+                $product[$index]["title"] = $row["title"];
+                $product[$index]["description"] = $row["description"];
+                $product[$index]["price"] = $row["price"];
+                $product[$index]["img"] = $row["img"];
+                $index++;
             }
             $result->close();
             $stmt->close();
         }
     }
 
-    //sending the mail with the products to the email Shop
+    //sending the mail with the products to the SHOP email
     if (isset($_POST["submit"])) {
-
         $from = $_POST["email"];
 
         //validation for the email
-        if (!valid_email($from)) {
-            echo "Please add a valid e-mail address!";
+        if(!boolval(filter_var($from, FILTER_VALIDATE_EMAIL))) {
+            echo t("Please add a valid e-mail adress!");
         } else {
             $name = $_POST["coustomer_name"];
+
+            // setting the headers for html mail sending
+            $headers='MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html;charset=iso-8859-1' . "\r\n";
             $subject = "Order list";
-            $message = "Coustomer ".$name." has the following order list: ";
 
             //composing the message with the products list
-            foreach ($_SESSION["cart"] as $key => $value) {
-                $message .= $_SESSION["products"][$_SESSION["cart"][$key]-1]["title"].", ";
+            $message = "
+            <html>
+            <head>
+                <title> Ordered products </title>
+            </head>
+            <body>
+                <p> Products list desired by coustomer:".$name."</p>
+                <p> E-mail adress of the coustomer:".$from."</p>
+                <table>
+                ";
+            foreach ($product as $key => $value) {
+                $message .= "
+                <tr>
+                       <td><img width='200' src='Images/".$product[$key]["img"]."' alt=''></td>
+                       <td>".$product[$key]["title"]."</td>
+                       <td>".$product[$key]["description"]."</td>
+                       <td>".$product[$key]["price"]."</td>
+                </tr>
+                ";
             }
-
-            $message .= "\n\n Additional information: "
-            .$_POST["comments"];
-            $headers = "From: ". $from;
-
+            $message .= "
+                </table>
+                <p> Additional information from the client :".$_POST["comments"]."
+            </body>
+            </html>
+            ";
+            //sending the mail
             if (mail(SHOP_EMAIL,$subject,$message,$headers)) {
 
                 //Releasing all the data from the cart as the mail was sent
@@ -69,11 +102,10 @@
                     }
                     $_SESSION["cart"] = array_values($_SESSION["cart"]);
                 }
-                unset($_SESSION["cart"][0]);
-                $_SESSION["cart"] = array_values($_SESSION["cart"]);
             }
         }
     }
+
  ?>
  <!DOCTYPE html PUBLIC>
     <html>
@@ -94,18 +126,18 @@
                         <th><?= t('Price'); ?></th>
                         <th> </th>
                     </tr>
-                    <?php foreach ($_SESSION["cart"] as $key => $value): ?>
-                        <tr>
-                            <td><img width="200" src="Images/<?= $_SESSION["products"][$_SESSION["cart"][$key]-1]["img"]; ?>" alt=""></td>
-                            <td><?= $_SESSION["products"][$_SESSION["cart"][$key]-1]["title"]; ?></td>
-                            <td><?= $_SESSION["products"][$_SESSION["cart"][$key]-1]["description"]; ?></td>
-                            <td><?= $_SESSION["products"][$_SESSION["cart"][$key]-1]["price"]; ?></td>
-                            <td><a href="cart.php?id=<?= $_SESSION["products"][$_SESSION["cart"][$key]-1]['id'] ?>"><?= t('Remove Item'); ?></a></td>
-                        </tr>
-                    <?php endforeach; ?>
+                    <?php foreach ($product as $key => $value): ?>
+                           <tr>
+                                  <td><img width="200" src="Images/<?= $product[$key]["img"]; ?>" alt=""></td>
+                                  <td><?= $product[$key]["title"]; ?></td>
+                                  <td><?= $product[$key]["description"]; ?></td>
+                                  <td><?= $product[$key]["price"]; ?></td>
+                                  <td><a href="cart.php?id=<?= $product[$key]['id'] ?>"><?= t('Remove Item'); ?></a></td>
+                           </tr>
+                   <?php endforeach; ?>
                 </table>
             <?php endif; ?>
-            <?php if (count($_SESSION["cart"]) <= 0): ?>
+            <?php if (!count($_SESSION["cart"])): ?>
                 <?= t("You have not selected items yet!"); ?>
             <?php endif; ?>
             <p><a href="index.php"> INDEX </a></p>
