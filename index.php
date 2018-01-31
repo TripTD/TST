acd2b0b5d6626f63a075b06178532f8ff3c794ee
TripTD <34335437+TripTD@users.noreply.github.com>
2018-01-31 11:24:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,14 +16,14 @@
 
         //preparing the sql statement and the parameters for binding
         if (!count($_SESSION["cart"])) {
-                
+
             $sql = "SELECT * FROM products";
             if ($stmt = $conn->prepare($sql)) {
                 $stmt->execute();
                 $result = $stmt->get_result();
             }
         } else {
-           
+
             //binding the parameters and selection part of the INDEX
             // type will be the data type for binding and params will be the reference array of the cart products
             if (!isset($type) && !isset($params)) {
@@ -35,7 +35,7 @@
                 $type .= "s";
                 $params[] = & $_SESSION["cart"][$i];
             }
-                
+
             $sql = "SELECT * FROM products WHERE id NOT IN (".str_repeat('?,',count($_SESSION['cart'])-1).'?'.")";
             if ($stmt = $conn->prepare($sql)) {
                 call_user_func_array(array($stmt,"bind_param"), array_merge(array($type),$params));
@@ -56,6 +56,13 @@
                 <p><a href="index.php?language=fr"><?= t('Francais'); ?></a></p>
                  <div id="main">
                      <table>
+                         <tr>
+                             <th>     </th>
+                             <th><?= t('Title'); ?></th>
+                             <th><?= t('Description'); ?></th>
+                             <th><?= t('Price'); ?></th>
+                             <th> </th>
+                         </tr>
                         <?php while ($row = $result->fetch_array(MYSQLI_ASSOC)): ?>
                                <tr>
                                       <td><img width="200" src="Images/<?= $row["img"]; ?>" alt=""></td>
@@ -70,7 +77,7 @@
                 </div>
                 <p><a href="cart.php"><?= t('Go to Cart'); ?></a></p>
             </div>
-            <?php if (!isset($_SESSION["logged"])): ?>
+            <?php if (!isset($_SESSION["logged"]) || !$_SESSION["logged"]): ?>
                 <p><a href="login.php"><?= t('Log in'); ?></a></p>
             <?php endif; ?>
        </body>
