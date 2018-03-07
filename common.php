c06b5aec6d922aba44d8cb6f0905b6eec711e7ef
TripTD <34335437+TripTD@users.noreply.github.com>
2018-03-07 16:40:48 +0200

    Update common.php

--- common.php
+++ common.php
@@ -1,34 +1,37 @@
 <?php
 
-    session_start();
-    require_once("config.php");
+session_start();
+require_once("config.php");
 
-    //connecting to the database
-    $conn = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+//connecting to the database
+$conn = new mysqli( DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }   
-        
-    //Initializing the cart array
-    if (!isset($_SESSION["cart"])) {
-        $_SESSION["cart"] = array();
-    }   
-        
-    //Translate function
-    function t($string, $langcode = NULL) {
-        require("translations.php");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        //checking for language. if not setted  then make it default the english
-        if (isset($_SESSION["langcode"])) {
-            if (isset($_GET["language"])) {
-                $_SESSION["langcode"] = $_GET["language"];
-            }
-        } else {
-            $_SESSION["langcode"] = "en";
-        }
-        if ( isset($translation[$_SESSION["langcode"]][$string]) ) {
-                $string = $translation[$_SESSION["langcode"]][$string];
-        }
-        return $string;
+//Initializing the cart array
+if (!isset($_SESSION["cart"])) {
+    $_SESSION["cart"] = array();
+}
+
+if (!isset($_SESSION["language"])) {
+    $_SESSION["language"] = "en";
+}
+if (isset($_GET["language"])) {
+    $_SESSION["language"] = $_GET["language"];
+}
+
+require_once("translations.php");
+
+//Translate function
+function t($string, $langcode = NULL) {
+    global $translations;
+
+    //checking for language. if not set  then make it default the english
+    if ( isset($translations[$_SESSION["language"]][$string]) ) {
+        $string = $translations[$_SESSION["language"]][$string];
     }
+
+    return $string;
+}
